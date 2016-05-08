56bd9cf2b705dc4c30ac57cae0bdb510c3ee958f
Akiva Lipshitz <aclscientist@gmail.com>
2016-05-08 08:24:39 -0400

    start script

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ var app = express()
 app.use(express.static(__dirname + '/public'));
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
